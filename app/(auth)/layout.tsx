3f36d2c015ddbd73644e44e07c5ac58a0cf8913b
Asif <babu033045@gmail.com>
2025-03-24 23:44:24 +0600

    Update code

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
 import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      {children}
+    </div>
+  );
 };
 
 export default AuthLayout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Page</div>;
+  return <div>HOme Page</div>;
 }
