f2f450ebec97df1347eaab0a757427acd0f42d88
Asif <babu033045@gmail.com>
2025-04-05 22:17:39 +0600

    Update code

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,7 @@
 import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="w-full h-screen flex justify-center items-center">
-      {children}
-    </div>
-  );
+  return <div className="auth-laout">{children}</div>;
 };
 
 export default AuthLayout;
---
+++ components/AuthForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthForm = ({ type }: { type: string }) => {
+  return (
+    <div className="card-border lg:min-w-[566px]">
+      <div className="card">Asif</div>
+    </div>
+  );
+};
+
+export default AuthForm;
